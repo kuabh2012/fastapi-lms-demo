5e48ad625b2ee4aa88e24e7a6b08f6507593abdf
abhishek.kumar <abhishek.kumar@BMDVABHISHEKKUM.local>
2022-08-28 16:35:52 +0530

    created routers for api endpoints

--- main.py
+++ main.py
@@ -1,44 +1,22 @@
-from ast import Str
-from optparse import Option
-from typing import Optional 
-from fastapi import FastAPI,Path, Query
+from typing import Optional, List
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
+from api import users, courses, sections
+
 app = FastAPI(
-    title="LMS With Fastapi Test",
-    description="LMS Mapping for students and courses.",
+    title="Fast API LMS",
+    description="LMS for managing students and courses.",
     version="0.0.1",
     contact={
-        "name": "abhi",
-        "email": "abhi@example.com",
+        "name": "Gwen",
+        "email": "gwen@example.com",
     },
     license_info={
         "name": "MIT",
     },
 )
 
-
-users = []
-
-class User(BaseModel):
-    email: str
-    is_active : bool
-    bio: Optional[str]
-
-@app.get("/users")
-async def get_users():
-    return users
-
-
-@app.post("/users")
-async def create_user(user:User):
-    users.append(user)
-    return "sucess"
-
-@app.get("/users/{id}")
-async def get_user(
-    id: int = Path(..., description="the id of the users you want to retrive" , gt=2),
-    q : str = Query(None , max_length=5)
-
-):
-    return {"user": users[id] , "query" : q}
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
